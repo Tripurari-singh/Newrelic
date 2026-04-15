8ce79d10148877f4d40f4c806d5516029da4df5c
Tripurari-singh <tripurari5821@gmail.com>
2026-04-15 12:54:34 +0530

    winston Added

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,35 @@
-import 'newrelic';
-import express from "express";
+// import 'newrelic';
+// import express from "express";
 
-const app = express();
+// const app = express();
 
-app.get("/" , (req , res) => {
-    console.log("Route Hit !")
-    res.json({
-        message : "Hello From Get Endpoint"
-    })
+// app.get("/" , (req , res) => {
+//     console.log("Route Hit !")
+//     res.json({
+//         message : "Hello From Get Endpoint"
+//     })
+// })
+
+// app.listen(3000 , () => {
+//     console.log("App is Listening On Port 3000")
+// })
+
+
+import winston from "winston";
+const logger = winston.createLogger({
+    level : 'silent',
+    format : winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+        winston.format.prettyPrint()
+    ),
+    transports : [
+        new winston.transports.File({ filename : 'error.log',level : 'error' }),
+        new winston.transports.File({ filename : 'combined.log' }),
+        new winston.transports.Console()
+    ]
 })
 
-app.listen(3000 , () => {
-    console.log("App is Listening On Port 3000")
-})
+logger.error(" Hello !")
+logger.info(" Hello !")
+logger.warn(" Hello !")
